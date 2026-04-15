4a33e22c38557a2aff28707e6821fc4c60293dd9
Nazrul Nazir <nazrul6290@gmail.com>
2026-04-15 14:27:56 +0600

    create pi chart

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { DiVim } from 'react-icons/di';
+import { WiMoonFull } from 'react-icons/wi';
+import { Pie, PieChart, Tooltip } from 'recharts';
 
-const Stats = () => {
+// Sample data
+// const data01 = [
+//   { name: 'Group A', value: 400 },
+//   { name: 'Group B', value: 300 },
+//   { name: 'Group C', value: 300 },
+//   { name: 'Group D', value: 200 },
+// ];
+
+const data02 = [
+  { name: 'text', value: 300, fill: '#244d3f' },
+  { name: 'call', value: 300, fill: '#7f37f5' },
+  { name: 'video', value: 300, fill: '#37a163' }
+];
+
+function Stats() {
   return (
-    <div>
-      Stats
+    <div className='px-20 mb-10'>
+      <h1 className='text-4xl font-bold text-neutral-800 mt-10 mb-5'>Friendship Analytics</h1>
+
+      <div className=' bg-base-100 px-10 py-8'>
+        <h3 className='text-2xl font-bold text-[#244d3f]'>By Interaction Type</h3>
+        <div className='flex flex-col justify-center items-center'>
+          <PieChart
+            width={500}
+            height={350}
+          >
+
+            <Pie
+              data={data02}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              innerRadius={80}
+              outerRadius={120}
+              paddingAngle={5}
+              fill="#000000"
+            // label
+            />
+
+            <Tooltip />
+          </PieChart>
+          <div className='flex gap-5'>
+            <div className='flex items-center gap-1'><span className='text-[#244d3f]'><WiMoonFull /></span> <span>Text</span></div>
+            <div className='flex items-center gap-1 '><span className='text-[#7f37f5]'><WiMoonFull /></span> <span>Call</span></div>
+            <div className='flex items-center gap-1 '><span className='text-[#37a163]'><WiMoonFull /></span> <span>Video</span></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Stats
+export default Stats;
