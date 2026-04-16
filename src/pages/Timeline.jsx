c8226595824a4cb16b8e029643f6491ed2c56e9f
Nazrul Nazir <nazrul6290@gmail.com>
2026-04-16 10:48:38 +0600

    timeline page filtering data and sort exact product show page

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,29 +1,29 @@
-import React, { useContext, useState} from 'react'
+import React, { useContext, useState } from 'react'
 import { UserContext } from '../context/UserContext';
 
 const Timeline = () => {
   const { allData } = useContext(UserContext);
   const users = allData.userData;
 
-const [catagory, setcatagory] = useState('');
+  const [catagory, setcatagory] = useState('');
 
-  const getValue = (value)=> {
+  const getValue = (value) => {
     setcatagory(value);
   }
-console.log(catagory)
-  const filters = users ? users.filter(user => user.catagory === catagory) : users;
+  console.log(catagory)
+  const filters = catagory === '' ? users : users.filter(user => user.catagory === catagory);
   console.log(filters)
-  
+
 
   return (
-    <div className='px-5 md:px-10 lg:px-20'>
+    <div className='px-5 md:px-10 lg:px-20 my-10'>
       <h1 className='text-5xl font-semibold'>Timeline</h1>
       <div className='my-5'>
         <select defaultValue="Color scheme" className="select select-base-300">
-          <option onClick={(e)=> getValue(e.target.value)} value={''}>All Issues</option>
-          <option onClick={(e)=> getValue(e.target.value)} value={'Call'}>Calls</option>
-          <option onClick={(e)=> getValue(e.target.value)} value={'Text'}>Texts</option>
-          <option onClick={(e)=> getValue(e.target.value)} value={'Video'}>Videos</option>
+          <option onClick={(e) => getValue(e.target.value)} value={''}>All Issues</option>
+          <option onClick={(e) => getValue(e.target.value)} value={'Call'}>Calls</option>
+          <option onClick={(e) => getValue(e.target.value)} value={'Text'}>Texts</option>
+          <option onClick={(e) => getValue(e.target.value)} value={'Video'}>Videos</option>
         </select>
       </div>
       <div>
